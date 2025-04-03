f1dc374a0f1216aeeec7282e2cba318a73968759
Ben Wachtel <bcwachtel@protonmail.com>
2025-04-03 15:23:31 -0400

    resolve merge conflicts bc im a dumbdumb

--- init.lua
+++ init.lua
@@ -669,11 +669,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-<<<<<<< HEAD
-        -- gopls = {},
-=======
         gopls = {},
->>>>>>> 9a7033f (add indent lines to the gutter)
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
